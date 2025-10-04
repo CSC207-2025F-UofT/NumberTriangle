b18c0c7e2d6e6387af79c9dddfe3e39fbbf3f566
PrabeerSohal <prabeersohal@gmail.com>
2025-10-03 23:53:58 -0400

    Update NumberTriangle.java

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * This is the provided NumberTriangle class to be used in this coding task.
@@ -110,26 +112,36 @@ public class NumberTriangle {
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
 
-        // TODO define any variables that you want to use to store things
+        List<NumberTriangle> prevRow = new ArrayList<>();
 
-        // will need to return the top of the NumberTriangle,
-        // so might want a variable for that.
         NumberTriangle top = null;
 
         String line = br.readLine();
         while (line != null) {
 
-            // remove when done; this line is included so running starter code prints the contents of the file
             System.out.println(line);
 
-            // TODO process the line
+            String[] nums = line.trim().split("\\s+");
 
-            //read the next line
-            line = br.readLine();
-        }
-        br.close();
-        return top;
-    }
+                        List<NumberTriangle> currRow = new ArrayList<>();
+                        for (String n : nums) {
+                            currRow.add(new NumberTriangle(Integer.parseInt(n)));
+                        }
+
+                        if (top == null) {
+                            top = currRow.get(0);
+                        } else {
+                            for (int i = 0; i < prevRow.size(); i++) {
+                                prevRow.get(i).setLeft(currRow.get(i));
+                                prevRow.get(i).setRight(currRow.get(i + 1));
+                            }
+                        }
+                        prevRow = currRow;
+                        line = br.readLine();
+                    }
+                    br.close();
+                    return top;
+                }
 
     public static void main(String[] args) throws IOException {
 
